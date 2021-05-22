c99aecd58a471a27527a9c3d1a4db2f271bbb143
secteam911 <67767549+secteam911@users.noreply.github.com>
2021-05-22 07:29:46 -0400

    Update __init__.py

--- server/webui/__init__.py
+++ server/webui/__init__.py
@@ -1,5 +1,7 @@
 import random
 import string
+import pyqrcode
+from io import  BytesIO
 from functools import wraps
 import hashlib
 from datetime import datetime
@@ -48,7 +50,8 @@ def index():
     return render_template('index.html')
 
 
-@webui.route('/login', methods=['GET', 'POST'])
+
+@webui.route('/login' , methods=['GET' , 'POST'])
 def login():
     admin_user = User.query.filter_by(username='admin').first()
     if not admin_user:
@@ -62,23 +65,27 @@ def login():
                 db.session.add(new_user)
                 db.session.commit()
                 flash('Password set successfully. Please log in.')
-                return redirect(url_for('webui.login'))
+                return redirect(url_for('webui.two_factor_setup'))
+            return redirect(url_for('webui.login'))
         return render_template('create_password.html')
     if request.method == 'POST':
         if request.form['password']:
                 password_hash = hashlib.sha256()
                 password_hash.update((admin_user.salt + request.form['password']).encode())
                 if admin_user.password == password_hash.hexdigest():
-                    session['username'] = 'admin'
-                    last_login_time =  admin_user.last_login_time
-                    last_login_ip = admin_user.last_login_ip
-                    admin_user.last_login_time = datetime.now()
-                    admin_user.last_login_ip = request.remote_addr
-                    db.session.commit()
-                    flash('Logged in successfully.') 
-                    if last_login_ip:
-                        flash('Last login from ' + last_login_ip + ' on ' + last_login_time.strftime("%d/%m/%y %H:%M"))
-                    return redirect(url_for('webui.index'))
+                    if  not admin_user.verify_totp(request.form['twofactor']):
+                        flash("Wrong Password or twofactor code ")
+                    else:
+                        session['username'] = 'admin'
+                        last_login_time =  admin_user.last_login_time
+                        last_login_ip = admin_user.last_login_ip
+                        admin_user.last_login_time = datetime.now()
+                        admin_user.last_login_ip = request.remote_addr
+                        db.session.commit()
+                        flash('Logged in successfully.')
+                        if last_login_ip:
+                            flash('Last login from ' + last_login_ip + ' on ' + last_login_time.strftime("%d/%m/%y %H:%M"))
+                        return redirect('agents')
                 else:
                     flash('Wrong passphrase')
     return render_template('login.html')
@@ -138,3 +145,51 @@ def rename_agent():
 @webui.route('/uploads/<path:path>')
 def uploads(path):
     return send_from_directory(current_app.config['UPLOAD_FOLDER'], path)
+
+
+
+
+
+
+@webui.route('/twofactor' )
+def two_factor_setup():
+    #if 'username' not in session:
+    #    return redirect(url_for('webui.home'))
+    user = User.query.filter_by(username='admin').first()
+    if user is None:
+        return redirect(url_for('webui.home'))
+    # since this page contains the sensitive qrcode, make sure the browser
+    # does not cache it
+    return render_template('two_factor_auth.html'), 200, {
+        'Cache-Control': 'no-cache, no-store, must-revalidate',
+        'Pragma': 'no-cache',
+        'Expires': '0'}
+
+
+
+
+
+
+
+
+
+
+@webui.route('/qrcode')
+def qrcode():
+
+    user = User.query.filter_by(username='admin').first()
+    if user is None:
+        abort(404)
+
+    # for added security, remove username from session
+    #del session['admin']
+
+    # render qrcode for FreeTOTP
+    url = pyqrcode.create(user.get_totp_uri())
+    stream = BytesIO()
+    url.svg(stream, scale=5)
+    return stream.getvalue(), 200, {
+        'Content-Type': 'image/svg+xml',
+        'Cache-Control': 'no-cache, no-store, must-revalidate',
+        'Pragma': 'no-cache',
+        'Expires': '0'}
